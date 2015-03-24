4f65646c92493f79df0718a9bd533e5e527a4003
jkalef <jkalef@gmail.com>
2015-03-23 20:08:56 -0700

    first commit.  added basic static-page routing and controllers

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+
+	#need to define the action for index
+	def index
+	end
+
+
+	#need to define the action for about
+	def about
+	end
+
+
+	#define the action for contact
+	def contact
+	end
+
+end
